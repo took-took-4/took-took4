5e46a8ea941592a2457dae013793c75bdc9870fb
Triwith <triwith.m@ku.th>
2017-07-24 08:54:50 +0700

    Second

---
+++ index.js
@@ -0,0 +1,28 @@
+function() main{
+
+  var linkMotion = ''
+  var linkRain = ''
+  var linkServo = ''
+  var linkGas = ''
+  var linkSwitch = ''
+  var linkLight = ''
+
+  
+  // setInterval(function() {
+  //     $.ajax({
+  //         url: linkDoor
+  //     }).done(function(data) {
+  //         console.log('done');
+  //         if (data == 1 || data == 2) {
+  //             $('#doorbox').val('Door : Open');
+  //             $('#doorbutton').val('open');
+  //         } else {
+  //             $('#doorbox').val('Door : Close');
+  //             $('#doorbutton').val('close');
+  //         }
+  //     }).fail(function() {
+  //         console.error('Fail to receive Door');
+  //     });
+  // }, 400);
+}
+$().ready(main());
