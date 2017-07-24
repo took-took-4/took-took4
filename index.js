3c4b8573e3ae7518dc7eeda83abfc6d73623c261
Triwith <triwith.m@ku.th>
2017-07-24 09:26:52 +0700

    add progress bar

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-function() main{
+function main() {
+    var linkMotion = ''
+    var linkRain = ''
+    var linkServo = ''
+    var linkGas = ''
+    var linkSwitch = ''
+    var linkLight = ''
 
-  var linkMotion = ''
-  var linkRain = ''
-  var linkServo = ''
-  var linkGas = ''
-  var linkSwitch = ''
-  var linkLight = ''
+    progress = setInterval(function()
+    {
+        if($('#progress .progress-text').text()=='0%'){
+        $('#progress .progress-text').text('70' + '%');
+        $('#progress .progress-bar').css({'width':'70'+'%'});
+    }}, 1000);
+    // setInterval(function() {
+    //         if (  $('#gasbar').attr('aria-valuenow')==40) {
+    //           $('#progress .progress-text').text(progression + '%');
+    //           $('#progress .progress-bar').css({'width':progression+'%'});
+    //         }}, 1000)
 
-  
-  // setInterval(function() {
-  //     $.ajax({
-  //         url: linkDoor
-  //     }).done(function(data) {
-  //         console.log('done');
-  //         if (data == 1 || data == 2) {
-  //             $('#doorbox').val('Door : Open');
-  //             $('#doorbutton').val('open');
-  //         } else {
-  //             $('#doorbox').val('Door : Close');
-  //             $('#doorbutton').val('close');
-  //         }
-  //     }).fail(function() {
-  //         console.error('Fail to receive Door');
-  //     });
-  // }, 400);
+    // setInterval(function() {
+    //     $.ajax({
+    //         url: linkDoor
+    //     }).done(function(data) {
+    //         console.log('done');
+    //         if (data == 1 || data == 2) {
+    //             $('#doorbox').val('Door : Open');
+    //             $('#doorbutton').val('open');
+    //         } else {
+    //             $('#doorbox').val('Door : Close');
+    //             $('#doorbutton').val('close');
+    //         }
+    //     }).fail(function() {
+    //         console.error('Fail to receive Door');
+    //     });
+    // }, 400);
 }
 $().ready(main());
