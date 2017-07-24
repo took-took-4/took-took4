b321959f23ab050d6b38ee4512491e535c690fa6
Dacharat Pankong <dacharat.p@ku.th>
2017-07-25 01:01:58 +0700

    add temp graph

--- raingraph.js
+++ raingraph.js
@@ -1,7 +1,5 @@
 function setupGraph(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) {
     var ctx = document.getElementById("myChart");
-    var linkDatabase = 'http://158.108.165.223/data/fame/database'
-    var tempData = {}
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -10,35 +8,43 @@ function setupGraph(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sund
                     label: 'Year 2016',
                     data: [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(0, 158, 15, 0.2)',
+                        'rgba(255, 158, 33, 0.2)',
+                        'rgba(33, 255, 247, 0.2)',
+                        'rgba(159, 0, 239, 0.2)',
+                        'rgba(255, 0, 0, 0.2)',
+                        'rgba(231, 252, 0, 0.2)',
+                        'rgba(247, 49, 155, 0.2)'
                     ],
                     borderColor: [
-                        'rgba(255,99,132,0.4)',
-                        'rgba(54, 162, 235, 0.4)',
-                        'rgba(255, 206, 86, 0.4)',
-                        'rgba(75, 192, 192, 0.4)',
-                        'rgba(255, 159, 64, 0.4)'
+                        'rgba(0,158,15,0.4)',
+                        'rgba(255, 158, 33, 0.4)',
+                        'rgba(33, 255, 247, 0.4)',
+                        'rgba(159, 0, 239, 0.4)',
+                        'rgba(255, 0, 0, 0.4)',
+                        'rgba(231, 252, 0, 0.4)',
+                        'rgba(247, 49, 155, 0.4)'
                     ],
                     borderWidth: 4
                 },{ label: 'Year 2017',
                 data: [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.8)',
-                    'rgba(54, 162, 235, 0.8)',
-                    'rgba(255, 206, 86, 0.8)',
-                    'rgba(75, 192, 192, 0.8)',
-                    'rgba(255, 159, 64, 0.8)'
+                    'rgba(0, 158, 15, 0.8)',
+                    'rgba(255, 158, 33, 0.8)',
+                    'rgba(33, 255, 247, 0.8)',
+                    'rgba(159, 0, 239, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(231, 252, 0, 0.8)',
+                    'rgba(247, 49, 155, 0.8)'
                 ],
                 borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(0,158,15,1)',
+                    'rgba(255, 158, 33, 1)',
+                    'rgba(33, 255, 247, 1)',
+                    'rgba(159, 0, 239, 1)',
+                    'rgba(255, 0, 0, 1)',
+                    'rgba(231, 252, 0, 1)',
+                    'rgba(247, 49, 155, 1)'
                 ],
                 borderWidth: 4
 
@@ -55,6 +61,6 @@ function setupGraph(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sund
     }});
 }
 function main() {
-    setupGraph(20, 17, 17, 18, 19, 16,20,15);
+    setupGraph(20,17,17,18,19,12,20);
 }
 $().ready(main())
